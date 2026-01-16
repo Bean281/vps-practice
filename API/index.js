6fbbfea29925a4d98af6153a227c7a26511f1f3e
Bumbobin <ndthung281@gmail.com>
2026-01-16 16:24:19 +0700

    update

--- API/index.js
+++ API/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ["http://localhost:5173","http://157.245.200.177:5173"],
+  origin: ["http://localhost:5173","http://139.59.252.90:5173"],
 }));
 
 app.use(express.json());
